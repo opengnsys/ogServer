646355d3c7df7d2cd15dd12f19722c4d9db2817f
Isabel Arrans <isabelarrans@gmail.com>
2020-12-15 18:26:31 +0100

    #915 Fix GET /client/info test

--- tests/units/test_0021_get_client_info.py
+++ tests/units/test_0021_get_client_info.py
@@ -6,11 +6,19 @@ class TestGetClientInfoMethods(unittest.TestCase):
     def setUp(self):
         self.url = 'http://localhost:8888/client/info'
         self.headers = {'Authorization' : '07b3bfe728954619b58f0107ad73acc1'}
-        self.json = { "client": ["192.168.56.12"] }
+        self.json = { "client": ["192.168.2.1"] }
 
     def test_get(self):
         returned = requests.get(self.url, headers=self.headers, json=self.json)
         self.assertEqual(returned.status_code, 200)
+
+    def test_get_no_payload(self):
+        returned = requests.get(self.url, headers=self.headers, json=None)
+        self.assertEqual(returned.status_code, 400)
+    def test_get_no_payload(self):
+        returned = requests.get(self.url, headers=self.headers, json={})
+        self.assertEqual(returned.status_code, 400)
+    
 
 if __name__ == '__main__':
     unittest.main()
