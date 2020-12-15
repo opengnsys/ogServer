10b07ba2b9f677074a9f860e618ba6865e83324c
Isabel Arrans <isabelarrans@gmail.com>
2020-12-15 18:27:37 +0100

    #915 Fix test POST /client/add

--- tests/units/test_0022_post_client_add.py
+++ tests/units/test_0022_post_client_add.py
@@ -28,13 +28,28 @@ class TestPostClientAddMethods(unittest.TestCase):
         returned = requests.post(self.url, headers=self.headers, json=self.json)
         self.assertEqual(returned.status_code, 200)
 
-    #def test_post_no_payload(self):
-        #returned = requests.post(self.url, headers=self.headers, json={})
-        #self.assertEqual(returned.status_code, 400)
+    def test_post_no_payload(self):
+        returned = requests.post(self.url, headers=self.headers, json=None)
+        self.assertEqual(returned.status_code, 400)
 
-    #def test_post_malformed_payload(self):
-        #returned = requests.post(self.url, headers=self.headers, json={'boot' :"19pxeADMIN"})
-        #self.assertEqual(returned.status_code, 400)
+    def test_post_malformed_payload(self):
+        returned = requests.post(self.url, headers=self.headers, json={'boot' :"19pxeADMIN",#
+                      "center": 0,
+                      "hardware_id": 0,
+                       "id": 2,
+                       "ip": "192.168.56.13",
+                       "livedir": "ogLive",
+                       "mac": "0800270E6512",
+                       "maintenance": True,
+                       "name": "pc12",
+                       "netdriver": "generic",
+                       "netiface": "eth1",
+                       "netmask": "255.255.255.0",
+                       "remote": False,
+                       "repo_id": 1,
+                       "room": 1,
+                       "serial_number": ""})
+        self.assertEqual(returned.status_code, 400)
 
 if __name__ == '__main__':
     unittest.main()
