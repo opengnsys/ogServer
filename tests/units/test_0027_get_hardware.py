1c2a0cad8b44757b6d1cb3c48527a757748e8978
Diego Crespo Quinta <diegocrespodcq98@gmail.com>
2021-03-22 21:23:07 +0100

    Fix GET /hardware test url

--- tests/units/test_0027_get_hardware.py
+++ tests/units/test_0027_get_hardware.py
@@ -4,9 +4,9 @@ import unittest
 class TestGetHardwareMethods(unittest.TestCase):
 
     def setUp(self):
-        self.url = 'http://localhost:8888/session'
+        self.url = 'http://localhost:8888/hardware'
         self.headers = {'Authorization' : '07b3bfe728954619b58f0107ad73acc1'}
-        self.json = { 'client' : [ '192.168.2.1' ] }
+        self.json = { 'client' : ['192.168.56.11'] }
 
     def test_get(self):
         returned = requests.get(self.url, headers=self.headers, json=self.json)
