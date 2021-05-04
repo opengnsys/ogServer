a7cce8d442a2c83562483ff3e649c95e6e732ea9
OpenGnSys Support Team <soporte-og@soleta.eu>
2021-05-04 17:59:11 +0200

    ogServer is AGPLv3+
    Update license header in files.

--- src/cfg.c
+++ src/cfg.c
@@ -1,9 +1,10 @@
 /*
- * Copyright (C) 2020 Soleta Networks <info@soleta.eu>
+ * Copyright (C) 2020-2021 Soleta Networks <info@soleta.eu>
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License as published by the
- * Free Software Foundation, version 3.
+ * Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
  */
 
 #include "json.h"
